f0c57a29327c0ff43bd9cac7dc14c4198fc8884b
eldhopj <eldho@live.in>
2019-10-26 22:07:43 +0530

    Registration validation checks finished ( except username)

--- app/src/main/java/com/example/rebuildkeralaandroid/ui/RegistrationActivity.java
+++ app/src/main/java/com/example/rebuildkeralaandroid/ui/RegistrationActivity.java
@@ -1,28 +1,47 @@
 package com.example.rebuildkeralaandroid.ui;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.rebuildkeralaandroid.R;
 import com.example.rebuildkeralaandroid.data.model.RegisterModel;
+import com.example.rebuildkeralaandroid.databinding.ActivityRegistrationBinding;
 import com.example.rebuildkeralaandroid.viewModel.RegisterViewModel;
+
+import java.util.Objects;
 
 public class RegistrationActivity extends AppCompatActivity {
     private static final String TAG = "RegistrationActivity";
     private RegisterViewModel viewModel;
+    ActivityRegistrationBinding binding;
+    private String userName;
+    private String emailID;
+    private String password1;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_registration);
         viewModel = ViewModelProviders.of(this).get(RegisterViewModel.class);
-
+//        binding.login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (!(validateEmail() & validateName() & validatePassword() & validateUserName())){
+//                    return;
+//                }
+//                createRegistrationObject();
+//            }
+//        });
     }
 
-    private void createNewRegistration(String userName, String email, String name, String password) {
-        registrationApi(new RegisterModel(userName, email, name, password));
+    private void createRegistrationObject() {
+        registrationApi(new RegisterModel(userName, emailID, name, password1));
     }
 
     private void registrationApi(RegisterModel registerModel) {
@@ -31,4 +50,64 @@ public class RegistrationActivity extends AppCompatActivity {
         });
     }
 
+    private boolean validateUserName() {
+        userName = binding.userName.getEditText().getText().toString().trim();
+        if (userName.isEmpty()) {
+            binding.userName.setError("Field can't be empty");
+            return false;
+        } else {
+            binding.userName.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateName() {
+        name = binding.name.getEditText().getText().toString().trim();
+        if (name.isEmpty()) {
+            binding.name.setError("Field can't be empty");
+            return false;
+        } else {
+            binding.name.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateEmail() {
+        emailID = binding.emailId.getEditText().getText().toString().trim();
+        if (!(Patterns.EMAIL_ADDRESS.matcher(emailID).matches())) {
+            binding.emailId.setError("Enter a valid Email Address");
+            return false;
+        } else {
+            binding.emailId.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        password1 = binding.password1.getEditText().getText().toString().trim();
+        String password2 = binding.password2.getEditText().getText().toString().trim();
+        if (password1.isEmpty()) {
+            binding.password1.setError("Field Cant be Empty");
+            return false;
+        } else if (password1.length() < 6) {
+            binding.password1.setError("Minimum 6 characters needed");
+            return false;
+        } else if (!Objects.equals(password1, password2))           // checking both passwords are similar
+        {
+            binding.password2.setError("Password Mismatches");
+            return false;
+        } else {
+            binding.password1.setError(null);
+            binding.password2.setError(null);
+            return true;
+        }
+
+    }
+
+    public void registerUser(View view) {
+        if (!(validateEmail() & validateName() & validatePassword() & validateUserName())) {
+            return;
+        }
+        createRegistrationObject();
+    }
 }
