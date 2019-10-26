74d065041fd925a0a9a5c8b700760c289e4c1097
Patrick Prakash <patrickprakash8@gmail.com>
2019-10-26 22:41:40 +0530

    Created Console Activity

---
+++ app/src/main/java/com/example/rebuildkeralaandroid/ui/ConsoleActivity.java
@@ -0,0 +1,18 @@
+package com.example.rebuildkeralaandroid.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rebuildkeralaandroid.R;
+
+public class ConsoleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camp_list);
+    }
+}
+
+
