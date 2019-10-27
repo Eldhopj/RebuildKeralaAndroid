4d77cac44bdeea65e13cdc86bf5ed8cf9012c819
sayoojtmc <sayoojtmc@gmail.com>
2019-10-27 07:05:21 +0530

    Added News RecyclerView

---
+++ app/src/main/java/com/example/rebuildkeralaandroid/ui/NewsActivity.java
@@ -0,0 +1,16 @@
+package com.example.rebuildkeralaandroid.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rebuildkeralaandroid.R;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
---
+++ app/src/main/java/com/example/rebuildkeralaandroid/ui/NewsAdapter.java
@@ -0,0 +1,36 @@
+package com.example.rebuildkeralaandroid.ui;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
+
+
+}
